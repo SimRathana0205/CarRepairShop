cea77aafd7e122fae52fccd25559a08bf25e5a9b
James1freedictionary <97018942+James1freedictionary@users.noreply.github.com>
2024-06-17 09:44:05 +0700

    Update LoginandSignup.java

---
+++ src/main/java/io/github/james1freedictionary/project/java/CarRepairShop/LoginandSignup.java
@@ -0,0 +1,10 @@
+public class LoginandSignup {
+    
+    public static void main(String[] args) {
+         Login LoginFrame= new Login(); 
+         LoginFrame.setVisible(true);
+         LoginFrame.pack();
+         LoginFrame.setLocationRelativeTo(null); // Center Form
+    }
+    
+}
